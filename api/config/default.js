8bceadb615627649da3fef1d803656539aae1024
Stefaan Ponnet <stefaan@ponnet.com>
2019-12-12 00:33:08 +0100

    update IPFS endpoint

--- api/config/default.js
+++ api/config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     environment: "development",
     databasefile: "data/reststore",
     authenticatedaccounts: ["0x00"],
-    ipfshost: { host: '23.254.227.151', port: 5001, protocol: 'http' },
+    ipfshost: { host: 'ipfs.web3.party', port: 5001, protocol: 'https' },
     ethers: {
         jsonproviderurl: "https://infura.io/U8U4n8mm2wDgB2e3Dksv",
         chainId: ethers.utils.getNetwork('homestead').chainId
